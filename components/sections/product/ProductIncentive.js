cf95b3641c6a719e3481aecdd704ee92aa133e27
eds2002 <es23jr@gmail.com>
2022-09-19 20:54:28 -0400

    design changes in product 2

--- components/sections/product/ProductIncentive.js
+++ components/sections/product/ProductIncentive.js
@@ -9,12 +9,12 @@ const ProductIncentive = () => {
       <div className = "relative w-full h-full">
         <Image src = "https://images.pexels.com/photos/981062/pexels-photo-981062.jpeg?cs=srgb&dl=pexels-dominic-buccilli-981062.jpg&fm=jpg" layout = 'fill' objectFit='cover' className = "rounded-md"/>
         <div className = "absolute inset-0 flex items-center justify-center rounded-md bg-black/40">
-          <div className = "flex flex-col items-center justify-center w-full max-w-sm">
+          <div className = "flex flex-col items-center justify-center w-full max-w-xs">
             <span className = "text-primaryVariant">~ Hufi</span>
             <h1 className = "text-xl `md:text-2xl text-background">We love our pets.</h1>
-            <p className = "text-sm text-center text-background/80 md:text-base">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, deserunt reiciendis. Velit sunt dolorem debitis eligendi cum dolorum, illum eum modi ea minus voluptate magni similique alias corrupti dolore nostrum!</p>
+            <p className = "text-sm text-center text-background/80 md:text-base">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, deserunt reiciendis. </p>
             <div className = "w-32 mt-4">
-              <Button text = 'Learn more' CSS = 'bg-background py-2'/>
+              <Button text = 'Learn more' CSS = 'bg-background py-1'/>
             </div>
           </div>
         </div>
