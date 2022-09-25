740fc954de40b916002a57a136cf0c1fbde9aca1
eds2002 <es23jr@gmail.com>
2022-09-24 21:18:52 -0400

    error fixes in rewards member page

--- pages/hufi-rewards-member.js
+++ pages/hufi-rewards-member.js
@@ -23,7 +23,7 @@ const RewardsMember = () => {
         </div>
         <div className = "relative z-[1] flex flex-col items-center justify-center pb-16 text-center">
           <h1 className = "max-w-sm text-3xl font-medium md:max-w-xl sm:text-4xl lg:text-5xl">Hufi Rewards Member</h1>
-          <p className = "max-w-md mt-2">The rewards are endless, and we're always adding more benefits for our loyal customers.</p>
+          <p className = "max-w-md mt-2">The rewards are endless, and we&apos;re always adding more benefits for our loyal customers.</p>
           <div className = "mt-4 text-base xl:text-lg ">
             <Link href = "/signup">
               <Button text = 'Become a member' CSS = {'px-4 py-2 bg-secondaryVariant hover:bg-secondary '}/>
@@ -43,7 +43,7 @@ const RewardsMember = () => {
             </div>
             <div className = "md:w-[50%] w-full flex items-start justify-center flex-col">
               <h1 className = "w-full max-w-md mt-4 text-xl font-medium md:ml-6">Free shipping on all orders.</h1>
-              <p className = "max-w-md mt-2 text-onBackground/70 md:ml-6">You don't have to meet the minimum spend of $75 when you're a rewards member. Make sure to use "Members Rewards" for free shipping on every purchase.</p>
+              <p className = "max-w-md mt-2 text-onBackground/70 md:ml-6">You don&apos;t have to meet the minimum spend of $75 when you&apos;re a rewards member. Make sure to use &quot;Members Rewards&quot; for free shipping on every purchase.</p>
             </div>
           </div>
           <div className = "flex-col md:flex-row-reverse max-w-7xl h-[50%] flex items-center justify-center mx-auto px-4 mb-6">
@@ -52,7 +52,7 @@ const RewardsMember = () => {
             </div>
             <div className = "md:w-[50%] w-full flex items-end justify-center flex-col">
               <h1 className = "w-full mt-4 text-xl font-medium md:max-w-md ">Occasional Discounts.</h1>
-              <p className = "mt-2 md:max-w-md text-onBackground/70">We love discounts, and we're sure you do too. We give occasional discounts ranging from 10%-30% off.</p>
+              <p className = "mt-2 md:max-w-md text-onBackground/70">We love discounts, and we&apos;re sure you do too. We give occasional discounts ranging from 10%-30% off.</p>
             </div>
           </div>
         </div>
@@ -65,7 +65,7 @@ const RewardsMember = () => {
               <Image src = "https://images.pexels.com/photos/45842/clasped-hands-comfort-hands-people-45842.jpeg?cs=srgb&dl=pexels-pixabay-45842.jpg&fm=jpg" alt = "Helping hand, 2 hands intertwined" objectFit='cover' layout='fill' className = "pointer-events-none select-none"/>
               <div className = "absolute inset-0 bg-black/30"/>
               <div className = "absolute inset-0 flex flex-col items-center justify-center max-w-sm mx-auto text-center">
-                <h1 className = "text-3xl font-medium text-background">We're a team.</h1>
+                <h1 className = "text-3xl font-medium text-background">We&apos;re a team.</h1>
                 <p className = "text-background/80">Be the first to know about new product launches. </p>
               </div>
             </div>
@@ -82,8 +82,8 @@ const RewardsMember = () => {
       </section>
       <section className = 'py-24'>
         <div className = "flex flex-col items-center justify-center px-4 mx-auto text-center max-w-7xl">
-          <h1 className = "max-w-md text-xl font-medium">We're making shopping easier for members.</h1>
-          <p className = "max-w-sm mt-2 text-base font-light text-onBackground/80">Stay tuned as we're always adding new benefits for our members.</p>
+          <h1 className = "max-w-md text-xl font-medium">We&apos;re making shopping easier for members.</h1>
+          <p className = "max-w-sm mt-2 text-base font-light text-onBackground/80">Stay tuned as we&apos;re always adding new benefits for our members.</p>
           <div className = "mt-6">
             <Link href = "/">
               <Button text = 'Start Shopping' CSS = 'bg-secondaryVariant py-2 hover:bg-secondary px-4'/>
