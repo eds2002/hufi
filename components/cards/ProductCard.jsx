5bc9c20572341d5d83fc11b0cb2bce26725e4791
eds2002 <es23jr@gmail.com>
2022-09-29 23:34:42 -0400

    fixed buttons on product cards, mobile issue

--- components/cards/ProductCard.jsx
+++ components/cards/ProductCard.jsx
@@ -156,9 +156,9 @@ function ActionsContainer({product, setSelectedProduct, setSelectedOption, selec
 
       {/* Buttons */}
       <div className = "flex flex-row items-center justify-center gap-3 pt-2 mx-auto mt-3 md:mx-0 md:flex-row md:pt-0">
-        <Button text = 'Add to cart' CSS = "md:w-auto w-full px-4 text-sm bg-secondaryVariant hover:bg-secondary py-2 text-background" onClick={()=>handleClick(product)}/>
+        <Button text = 'Add to cart' CSS = "md:w-auto sm:px-4 w-full text-sm bg-secondaryVariant hover:bg-secondary py-2 text-background" onClick={()=>handleClick(product)}/>
         <Link href = {`/product/${product?.handle}`}>
-          <Button text = {"Details"} CSS = 'md:w-auto px-4 text-sm bg-surface py-2 ring-1 ring-black text-onBackground font-medium bg-white'/>
+          <Button text = {"Details"} CSS = 'md:w-auto text-sm bg-surface sm:px-4 py-2 ring-1 ring-black text-onBackground font-medium bg-white'/>
         </Link>
       </div>
     </div>
