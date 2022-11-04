7f784abc65c8a325bdbec982d9b3f1c231d8af89
eds2002 <es23jr@gmail.com>
2022-11-04 17:35:58 -0400

    design changes to product banner

--- components/sections/product/ProductBanner.jsx
+++ components/sections/product/ProductBanner.jsx
@@ -10,8 +10,8 @@ const ProductBanner = ({data}) => {
           <div className = "relative flex flex-col justify-center w-full p-6 overflow-hidden rounded-md aspect-[16/9] md:aspect-[16/7] lg:aspect-[16/5] bg-neutral-300">
             <div className = "absolute inset-0 z-10 rounded-md opacity-80 boxShadow"/>
             <Image src = {dataJSON.image} layout = 'fill' objectFit='cover' objectPosition={dataJSON.imagePos} className = "pointer-events-none select-none z-[1] rounded-md" priority/>
-            <h1 className = "relative z-10 text-lg text-white sm:text-2xl md:text-4xl">{dataJSON.heading}</h1>
-            <p className = "relative z-10 max-w-xs mt-2 text-sm text-white/90 lg:text-lg ">{dataJSON.paragraph}</p>
+            <h1 className = "relative z-10 text-xl font-medium text-white sm:text-2xl md:text-4xl">{dataJSON.heading}</h1>
+            <p className = "relative z-10 max-w-xs mt-2 text-sm text-white/90 md:text-base lg:text-lg ">{dataJSON.paragraph}</p>
             {/* PLACEHOLDER */}
             <p className = "absolute inset-0 flex items-center justify-center">
               <span className = "font-extrabold text-8xl text-neutral-200 animate-pulse">Hufi.</span>
